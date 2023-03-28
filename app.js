297a41fc7875a65cc2436adc719fb7b1022e589e
jaydeep4321 <jaydeepvaghela.thetatechnolabs@gmail.com>
2023-03-28 17:20:59 +0530

    -updated for deployment

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const csp = require('express-csp');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorcontroller = require('./controllers/errorController');
@@ -27,58 +29,13 @@ app.set('views', path.join(__dirname, 'views'));
 
 //1) GLOBAL MIDDLEWARES
 
+app.use(cors());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 //set security for HTTP header
 // app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
-
-// app.use(
-//   helmet({
-//     crossOriginEmbedderPolicy: false,
-//   })
-// );
-
-// // Further HELMET configuration for Security Policy (CSP)
-// const scriptSrcUrls = [
-//   'https://api.tiles.mapbox.com/',
-//   'https://api.mapbox.com/',
-//   'https://*.cloudflare.com',
-//   'https://js.stripe.com/v3/',
-//   'https://checkout.stripe.com',
-// ];
-// const styleSrcUrls = [
-//   'https://api.mapbox.com/',
-//   'https://api.tiles.mapbox.com/',
-//   'https://fonts.googleapis.com/',
-//   'https://www.myfonts.com/fonts/radomir-tinkov/gilroy/*',
-//   ' checkout.stripe.com',
-// ];
-// const connectSrcUrls = [
-//   'https://*.mapbox.com/',
-//   'https://*.cloudflare.com',
-//   'http://127.0.0.1:3000',
-//   'http://127.0.0.1:52191',
-//   '*.stripe.com',
-// ];
-
-// const fontSrcUrls = ['fonts.googleapis.com', 'fonts.gstatic.com'];
-
-// app.use(
-//   helmet.contentSecurityPolicy({
-//     directives: {
-//       defaultSrc: [],
-//       connectSrc: ["'self'", ...connectSrcUrls],
-//       scriptSrc: ["'self'", ...scriptSrcUrls],
-//       styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-//       workerSrc: ["'self'", 'blob:'],
-//       objectSrc: [],
-//       imgSrc: ["'self'", 'blob:', 'data:'],
-//       fontSrc: ["'self'", ...fontSrcUrls],
-//       frameSrc: ['*.stripe.com', '*.stripe.network'],
-//     },
-//   })
-// );
 
 //development logging
 if (process.env.NODE_ENV === 'development') {
--- public/js/bundle.js
+++ public/js/bundle.js
@@ -69058,7 +69058,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40211" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41065" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
